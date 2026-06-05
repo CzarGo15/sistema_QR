5cfb2ac57ef2cf45abff1712a9a505928d2f98a4
CzarGo15 <hellfish335@gmail.com>
2026-06-05 13:48:23 -0600

    Update firebase.js

--- backend/firebase.js
+++ backend/firebase.js
@@ -1,6 +1,8 @@
 const admin = require('firebase-admin');
 
-const serviceAccount = require('./sistemaqr-a4d32-firebase-adminsdk-fbsvc-e5a2c6cb40.json');
+const serviceAccount = JSON.parse(
+    process.env.FIREBASE_SERVICE_ACCOUNT
+);
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
