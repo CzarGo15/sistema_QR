69414549adbbd04cad6ab8ef4d4295f030e5eccb
CzarGo15 <hellfish335@gmail.com>
2026-06-12 14:54:07 -0600

    Fix formatting and clean up firebase.js

--- backend/firebase.js
+++ backend/firebase.js
@@ -1,15 +1,15 @@
 const admin = require('firebase-admin');
 
-
 const serviceAccount = JSON.parse(
-    process.env.FIREBASE_SERVICE_ACCOUNT
+process.env.FIREBASE_SERVICE_ACCOUNT
 );
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
+credential: admin.credential.cert(serviceAccount)
+});
 
+const db = admin.firestore();
 
 console.log('✅ Firebase conectado correctamente');
 
-module.exports = db;
 module.exports = db;
