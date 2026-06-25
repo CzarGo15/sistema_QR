07c8b07822f4611b78eba7683a748a62c83f6b98
CzarGo15 <hellfish335@gmail.com>
2026-06-25 12:57:27 -0600

    Refactor image size import and usage

--- backend/services/pdf.js
+++ backend/services/pdf.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 const axios = require('axios');
 const bwipjs = require('bwip-js');
-const sizeOf = require('image-size');
+const { imageSize } = require('image-size');
 /*
 ==================================================
 CONFIGURACIÓN GENERAL
@@ -72,7 +72,7 @@ async function descargarImagen(url){
 
         const buffer = Buffer.from(response.data);
 
-        const dimensions = sizeOf(buffer);
+        const dimensions = imageSize(buffer);
 
         return {
 
